4149239ebc10b1df5eb06eabbb19e361ca3b6fb8
dom000 <godknowsegiboy00@gmail.com>
2024-06-03 20:18:49 +0100

    refactor: Update Faq component layout and styles
    Refactor the layout and styles of the Faq component in the Home page. Update the background color and remove unnecessary classes for better responsiveness.

--- src/components/sections/Home/Faq.tsx
+++ src/components/sections/Home/Faq.tsx
@@ -5,48 +5,48 @@ const Faq = () => {
   const [active, setActive] = useState(-1);
   return (
     <section className="md:-mt-9 -mt-5 z-30 relative 2xl:mx-auto 2xl:mx-w-7xl">
-      <div className="bg-zwilt-gray-100 w-full  polygon h-8 md:h-12"></div>
-      <div className="bg-zwilt-gray-100 w-full flex -mt-1 pb-14 md:pb-32  pt-16">
+      <div className="bg-punch_offwhite w-full  h-8 md:h-12"></div>
+      <div className="bg-punch_offwhite w-full flex -mt-1 pb-14 md:pb-32  pt-16">
         <div className="w-full">
           <div className="flex flex-wrap justify-center border-b ">
-            {faqContent.map((faq, idx) => (
+            {content.map((faq, idx) => (
               <div className="w-full" key={idx}>
-                {faq.questions.map((ques, i) => (
+                {faq.questions.map((question, index) => (
                   <div
-                    onMouseEnter={() => setActive(ques.id)}
+                    onMouseEnter={() => setActive(question.id)}
                     onMouseLeave={() => setActive(-1)}
                     className="border-t border-gray-300 flex flex-col md:flex-row items-center relative"
-                    key={i}
+                    key={index}
                   >
                     <div className="md:w-1/3 md:flex  hidden">
                       <p
                         className={`w-full md:w-56 text-lg font-semibold border-r border-gray-300 text-center py-4 ${
-                          ques.category ? "md:block hidden" : "hidden"
+                          question.category ? "md:block hidden" : "hidden"
                         }`}
                       >
-                        {ques.category}
+                        {question.category}
                       </p>
                       <p
                         className={`w-full md:w-56 text-lg font-semibold text-center py-4 ${
-                          ques.sub_category ? "block " : "hidden"
+                          question.sub_category ? "block " : "hidden"
                         } ${
-                          ques.sub_category ? "border-r border-gray-300" : ""
+                          question.sub_category ? "border-r border-gray-300" : ""
                         }`}
                       >
-                        {ques.sub_category}
+                        {question.sub_category}
                       </p>
                     </div>
                     <div className="flex-1 md:w-2/3 flex justify-between items-center px-6 py-4 hover:bg-gray-200 cursor-pointer transition-colors duration-300">
                       <p
                         className={`text-lg text-center md:text-left text-zwilt-text-100 ${
-                          active === ques.id ? "font-semibold" : ""
+                          active === question.id ? "font-semibold" : ""
                         }`}
                       >
-                        {ques.question}
+                        {question.question}
                       </p>
                       <img
                         className={` ${
-                          active === ques.id ? "opacity-1" : "opacity-0"
+                          active === question.id ? "opacity-1" : "opacity-0"
                         } transition-opacity duration-300  md:block hidden`}
                         src={"/assets/faqicon.svg"}
                         alt="logo"
@@ -65,7 +65,7 @@ const Faq = () => {
 
 export default Faq;
 
-export const faqContent: Content[] = [
+export const content: Content[] = [
   {
     questions: [
       {
